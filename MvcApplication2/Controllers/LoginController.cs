158c810120bebbceebb13edd41ae99c2454beab8
Richard Shelton-Davies <info@allcomputersolutions.co.uk>
2016-04-29 10:58:30 +0100

    Initial Commit

---
+++ MvcApplication2/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using SimpleBlog.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index (AuthLogin form)
+        {
+            return Content("Hello there " + form.Username +", how are you today?");
+        }
+
+    }
+}
---
+++ MvcApplication2/ViewModel/auth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.ViewModel
+{
+    public class AuthLogin
+    {
+        public string Username { get; set; }
+        public Int32 Password { get; set; }
+    }
+}
